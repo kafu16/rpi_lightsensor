740d891f938c4f87bdc9e76be0c5d070b232ca48
kafu16 <24458047+kafu16@users.noreply.github.com>
2022-12-14 16:08:56 +0100

    print messages

--- script_on_pi.sh
+++ script_on_pi.sh
@@ -3,9 +3,13 @@
 
 # go to directory of repo
 cd rpi_lightsensor
+echo "UPDATING LOCAL REPO (if not up to date with its remote counterpart)."
+echo ""
 git pull
 
+echo "STARTING MEASUREMENT"
 # execute script that writes sensor data to .csv-file
 python3 write_from_tsl2591.py
 
+echo "MEASUREMENT FINISHED"
 exit
